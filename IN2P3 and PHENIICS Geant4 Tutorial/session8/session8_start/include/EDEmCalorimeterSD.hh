b15de101a7000c418d53148c256b3e4b1a6bf9f2
lc-leo <35031807+lc-leo@users.noreply.github.com>
2020-06-22 08:09:49 -0500

    Add files via upload

---
+++ IN2P3 and PHENIICS Geant4 Tutorial/session8/session8_start/include/EDEmCalorimeterSD.hh
@@ -0,0 +1,61 @@
+//
+// ********************************************************************
+// * License and Disclaimer                                           *
+// *                                                                  *
+// * The  Geant4 software  is  copyright of the Copyright Holders  of *
+// * the Geant4 Collaboration.  It is provided  under  the terms  and *
+// * conditions of the Geant4 Software License,  included in the file *
+// * LICENSE and available at  http://cern.ch/geant4/license .  These *
+// * include a list of copyright holders.                             *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.  Please see the license in the file  LICENSE  and URL above *
+// * for the full disclaimer and the limitation of liability.         *
+// *                                                                  *
+// * This  code  implementation is the result of  the  scientific and *
+// * technical work of the GEANT4 collaboration.                      *
+// * By using,  copying,  modifying or  distributing the software (or *
+// * any work based  on the software)  you  agree  to acknowledge its *
+// * use  in  resulting  scientific  publications,  and indicate your *
+// * acceptance of all terms of the Geant4 Software license.          *
+// ********************************************************************
+//
+// $Id$
+//
+/// \file EDEmCalorimeterSD.hh
+/// \brief Definition of the EDEmCalorimeterSD class
+//
+
+#ifndef EDEmCalorimeterSD_h
+#define EDEmCalorimeterSD_h 1
+
+#include "G4VSensitiveDetector.hh"
+#include "EDEmCalorimeterHit.hh"
+
+class G4Step;
+class G4HCofThisEvent;
+class G4TouchableHistory;
+
+class EDEmCalorimeterSD : public G4VSensitiveDetector
+{
+  public:
+    EDEmCalorimeterSD(const G4String& name, 
+                      const G4String& hitsCollectionName);
+    virtual ~EDEmCalorimeterSD();
+
+    virtual void   Initialize(G4HCofThisEvent* hce);
+    virtual G4bool ProcessHits(G4Step* step, G4TouchableHistory* history);
+    virtual void   EndOfEvent(G4HCofThisEvent* hce);
+
+  private:
+    EDEmCalorimeterHitsCollection* fHitsCollection;
+};
+
+
+
+
+#endif
+
---
+++ IN2P3 and PHENIICS Geant4 Tutorial/session8/session8_start/src/EDMagneticField.cc
@@ -0,0 +1,65 @@
+//
+// ********************************************************************
+// * License and Disclaimer                                           *
+// *                                                                  *
+// * The  Geant4 software  is  copyright of the Copyright Holders  of *
+// * the Geant4 Collaboration.  It is provided  under  the terms  and *
+// * conditions of the Geant4 Software License,  included in the file *
+// * LICENSE and available at  http://cern.ch/geant4/license .  These *
+// * include a list of copyright holders.                             *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.  Please see the license in the file  LICENSE  and URL above *
+// * for the full disclaimer and the limitation of liability.         *
+// *                                                                  *
+// * This  code  implementation is the result of  the  scientific and *
+// * technical work of the GEANT4 collaboration.                      *
+// * By using,  copying,  modifying or  distributing the software (or *
+// * any work based  on the software)  you  agree  to acknowledge its *
+// * use  in  resulting  scientific  publications,  and indicate your *
+// * acceptance of all terms of the Geant4 Software license.          *
+// ********************************************************************
+//
+// $Id$
+//
+/// \file EDMagneticField.cc
+/// \brief Implementation of the EDMagneticField class
+
+#include "EDMagneticField.hh"
+
+#include "G4FieldManager.hh"
+#include "G4TransportationManager.hh"
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+EDMagneticField::EDMagneticField()
+  : G4MagneticField()
+{}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+EDMagneticField::~EDMagneticField()
+{}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+void EDMagneticField::GetFieldValue(const G4double /*point*/[4], 
+                                    G4double* bfield) const
+{
+  // Change this function to return the field value
+  bfield[0] = bfield[1] = bfield[2] = 0;
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+// Set new value of the field to fieldValue along Y
+
+void EDMagneticField::SetMagFieldValue(G4double /*fieldValue*/)
+{
+  // To be implemented
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
